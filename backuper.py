8978ddb8d2e8a904bf45c5aaf2bd4db0b9742fb9
hard1mpulse <linkup811@gmail.com>
2022-06-30 06:13:10 +0300

    Added directory and backup file creator

--- backuper.py
+++ backuper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import csv
 import time
+import os
 from routeros_ssh_connector import MikrotikDevice
 
 def read_data_from_csv(path):
@@ -24,10 +25,19 @@ def get_backup_from_mikrotik(ip,creds):
 ### Default paths of data
 devices_path='data/devices.csv'
 credentials_path='data/credentials.csv'
+backups_path='backups/'
+logs_path='logs/'
 
 devices=read_data_from_csv(devices_path)
 credentials=read_data_from_csv(credentials_path)
 
 for device in devices.keys():
     backup=get_backup_from_mikrotik(devices[device][2],credentials[devices[device][3]][1:3])
-    print(backup)
+    try:
+        os.mkdir(backups_path+device)
+    except FileExistsError:
+        pass
+    currentdate=time.strftime("%D")
+    filename=backups_path+device+'/'+currentdate.replace('/','_')+'_'+device+'.backup'
+    with open(filename,'w') as f:
+        f.write(backup)
