419dfcf6a133388ff26e633a26319c02554ff7dd
hard1mpulse <linkup811@gmail.com>
2022-07-01 13:59:44 +0300

    Added config.py with datapaths

---
+++ config.py
@@ -0,0 +1,5 @@
+### Default paths of data
+devices_path=os.getcwd()+'/'+'data/devices.csv'
+credentials_path=os.getcwd()+'/'+'data/credentials.csv'
+backups_path=os.getcwd()+'/'+'backups/'
+logs_path=os.getcwd()+'/'+'logs/'
